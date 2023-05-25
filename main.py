6fa5288db5b5220aaeb21430c077ed17a9cd2854
MCTerminal <2825-MCTerminal@users.noreply.replit.com>
2023-05-25 15:59:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index(name=None):
+  return render_template('index.html', name="Lucas")
+
+
+app.run(host='0.0.0.0', port=81)
